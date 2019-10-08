dc2d26b95774086fe16d37aad7b3c42e08bbff06
Jan Löwenstrom <jan.loewenstrom@web.de>
2019-10-08 14:39:47 +0200

    init

---
+++ backend/src/main/java/net/lwenstrom/musicquizbackend/MusicQuizBackendApplication.java
@@ -0,0 +1,13 @@
+package net.lwenstrom.musicquizbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicQuizBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicQuizBackendApplication.class, args);
+	}
+
+}
